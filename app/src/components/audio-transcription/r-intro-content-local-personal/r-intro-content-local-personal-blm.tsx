04de14a15a738d2d29b01788fa349d32bc8595ba
Tim Weiland <tim@weiland-lahnstein.de>
2020-08-10 01:06:01 +0200

    Replace 1 intro local audio

--- app/src/components/audio-transcription/r-intro-content-local-personal/r-intro-content-local-personal-blm.tsx
+++ app/src/components/audio-transcription/r-intro-content-local-personal/r-intro-content-local-personal-blm.tsx
@@ -4,7 +4,8 @@ import React from 'react';
 const RIntroContentLocalPersonalTranscriptionBLM: React.FC = () => (
   <>
     <h5>Beginne hier...</h5>
-    <p>Hey. Du hast also ein Geschenk bekommen. Schön. Jemand hat an dich gedacht. Geh einfach los, langsam, egal wohin und ich erkläre alles Weitere. OK?Jemand, den du kennst, hat das Museum besucht. Die Person hat Objekte ausgesucht, von denen sie glaubt, dass du sie mögen wirst. Sie hat Fotos gemacht und Nachrichten dazu aufgenommen. Schau, ob du die Objekte finden kannst. Wenn du nicht weiterkommst, zeige das Foto jemandem vom Museum. Eine der Aufsichten wird wissen, wo es sich befindet. Ich sehe dich auf der anderen Seite.</p>
+    <p>Hey. Du hast also ein Geschenk bekommen. Schön. Jemand hat an dich gedacht. Geh einfach los, langsam, egal wohin und ich erkläre alles Weitere. OK? </p>
+      <p>Jemand, den du kennst, hat das Museum besucht. Die Person hat Objekte ausgesucht, von denen sie glaubt, dass du sie mögen wirst. Sie hat Fotos gemacht und Nachrichten dazu aufgenommen. Schau, ob du die Objekte finden kannst. Wenn du nicht weiterkommst, zeige das Foto jemandem vom Museum. Ich sehe dich auf der anderen Seite.</p>
   </>
 );
 
